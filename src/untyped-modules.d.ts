b3fd788a5f906ce62706975fc850a5a3ded30676
Hugo McPhee <hugo.mcphee.public@gmail.com>
2021-12-19 15:23:23 +1100

    fixed build and added some apple html :bug:

---
+++ src/untyped-modules.d.ts
@@ -0,0 +1,14 @@
+declare module "*.webm" {
+  // const src: string;
+  // export default src;
+}
+
+declare module "*.mp4" {}
+
+declare module "*.mp3" {}
+
+declare module "*.glb" {}
+
+declare module "*.hdr" {}
+
+declare module "*.env" {}
